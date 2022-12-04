5040ab1964ed6bc138956c99fa0c8f51a2bddeaf
davcki <113711790+davcki@users.noreply.github.com>
2022-12-04 20:34:04 +0100

    Update regions_consumption.py

--- Project/Map/regions_consumption.py
+++ Project/Map/regions_consumption.py
@@ -1,40 +1,64 @@
 import pandas as pd
 import numpy as np
-import os
-import pooch
 
 
-url_cons = 'https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B'
-path_target = './consommation-annuelle-residentielle-par-adresse.csv'
-path, fname = os.path.split(path_target)
-pooch.retrieve(url_cons, path = path, fname = fname, known_hash = None)
-selected = ['Code INSEE de la commune', 'Consommation annuelle moyenne de la commune (MWh)']
-df_cons = pd.read_csv('consommation-annuelle-residentielle-par-adresse.csv', usecols = selected, sep = ";", float_precision = 'legacy')
-df_cons = df_cons.drop_duplicates().reset_index(drop = True)
-df_cons.rename(columns = {'Code INSEE de la commune':'COM', 'Consommation annuelle moyenne de la commune (MWh)':'Conso'}, inplace = True)
+class ConsumptionBy:
+
+    """
+    This class create a dataframe which provide
+    the French electricity consumption from 2019 to 2021
+    by department or regions.
+    It uses two data from the following websites:
+    
+    INSEE data:
+    https://www.insee.fr/fr/information/5057840
+
+    ENEDIS data:
+    https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/information/
+    
+    """
+
+    def getDataFast(COL):
+
+        """ Please use this attribute to get the data quickly from
+        the repository """
+
+        if COL == 'DEP':
+            data = pd.read_csv('./Map/Dataset/departements_consumption.csv', sep = ';')
+
+        elif COL == 'REG':
+            data = pd.read_csv('./Map/Dataset/regions_consumption.csv', sep = ';')
+
+        else: raise Exception("please give any of the following values as arguments : 'DEP' or 'REG' ")
+
+        return data
 
 
-url_com = 'https://www.insee.fr/fr/statistiques/fichier/5057840/commune2021-csv.zip'
-path_target2 = './commune2021-csv.zip'
-path2, fname2 = os.path.split(path_target2)
-pooch.retrieve(url_com, path = path2, fname = fname2, known_hash = None)
-COL = 'DEP'
-selected2 = ['COM', COL]
-df_2 = pd.read_csv('./commune2021-csv.zip', compression='zip', usecols = selected2, sep = ",")
+    def getDataSlow(COL):
 
+        """ Be Carreful! This attribute download the data from the
+        internet, the data will be up to date but it can take around
+        8 minutes """
 
-df_cons[['COM']] = df_cons[['COM']].astype(str)
+        url_cons = 'https://data.enedis.fr/explore/dataset/consommation-annuelle-residentielle-par-adresse/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B'
+        selected = ['Code INSEE de la commune', 'Consommation annuelle moyenne de la commune (MWh)']
+        df_cons = pd.read_csv(url_cons, usecols = selected, sep = ";", float_precision = 'legacy')
+        df_cons = df_cons.drop_duplicates().reset_index(drop = True)
+        df_cons.rename(columns = {'Code INSEE de la commune':'COM', 'Consommation annuelle moyenne de la commune (MWh)':'Conso'}, inplace = True)
+        
+        url_com = 'https://www.insee.fr/fr/statistiques/fichier/5057840/commune2021-csv.zip'
 
-conso_col = pd.merge(df_cons, df_2, on='COM')
-conso_col = conso_col[['Conso', COL]]
-conso_col = conso_col.dropna()
-conso_col.round({'Conso': 2})
+        selected2 = ['COM', COL]
+        df_2 = pd.read_csv(url_com, compression='zip', usecols = selected2, sep = ",")
 
-conso_col = conso_col.groupby([COL])['Conso'].mean().reset_index()
+        df_cons[['COM']] = df_cons[['COM']].astype(str)
 
-conso_col[COL] = conso_col[COL].astype(int)
-conso_col[COL] = conso_col[COL].astype(str)
+        conso_col = pd.merge(df_cons, df_2, on='COM')
+        conso_col = conso_col[['Conso', COL]]
+        conso_col = conso_col.dropna()
+        conso_col.round({'Conso': 2})
+        conso_col = conso_col.groupby([COL])['Conso'].mean().reset_index()
+        conso_col[COL] = conso_col[COL].astype(int)
+        conso_col[COL] = conso_col[COL].astype(str)
 
-
-conso_col.to_csv('conso_dep.csv', index=False)
-
+        return conso_col
